7d611575a047ed86b57f49781fa568592aacc579
jack-champlain <jack.clark@mymail.champlain.edu>
2024-03-09 14:26:36 -0500

    ssh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author jack-champlain
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes a roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
