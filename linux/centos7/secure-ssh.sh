3a3baa90f13b2bc96debf0b13a942aa5fd8bbf04
jack-champlain <jack.clark@mymail.champlain.edu>
2024-03-13 18:53:41 +0000

    oops

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,3 +4,9 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes a roots ability to ssh in
 echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/SYS265 -s /bin/bash testinguser
+sudo mkdir /home/SYS265/.ssh
+sudo cp  /home/admin/Sys-265/linux/public-keys/id_rsa.pub /home/SYS265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh/authorized_keys
+sudo chown -R testinguser:testinguser /home/SYS265/.ssh
+
